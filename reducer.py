0023fc744695da5e756eb10cf29be726edf0c6fd
kushalkatari <35985697+kushalkatari@users.noreply.github.com>
2022-02-04 08:55:27 -0600

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,1 +1,24 @@
+import sys
 
+thisKey = ""
+thisValue = 0.0
+
+for line in sys.stdin:
+  datalist = line.strip().split('\t')
+  if (len(datalist) == 2) : 
+    user rating score, count = datalist
+
+    if user rating score != thisKey:   # we've moved to another key
+      if thisKey:
+        # output the previous key-summaryvalue result
+        print(thisKey,'\t',thisValue)
+
+      # start over for each new key
+      thisKey = user rating score 
+      thisValue = 0.0
+  
+    # apply the aggregation function
+    thisValue += float(count)
+
+# output the final key-summaryvalue result outside the loop
+print(thisKey,'\t',thisValue)
