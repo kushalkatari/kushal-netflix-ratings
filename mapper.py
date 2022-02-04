3a4a9451f150005b365754e0cc6ce3c4f7abcb75
kushalkatari <35985697+kushalkatari@users.noreply.github.com>
2022-02-04 08:52:11 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,1 +1,10 @@
+import sys 
 
+# iterate through each line provided via standard input
+for line in sys.stdin:
+  datalist = line.strip().split(",")
+  if (len(datalist) ==2) : 
+    title,user rating score = datalist
+
+    # print intermediate key-value pairs to standard output
+    print(user rating score,"\t",1)
